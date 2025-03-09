9903139bebe7d0d0148ab0094d74f9f4b058cc59
Ahsan Abrar <iahsanabrar@gmail.com>
2025-03-09 14:44:38 +0500

    new

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -3,3 +3,7 @@ import { sum } from '../src/index';
 test('adds 5 + 7 to equal 12', () => {
     expect(sum(5, 7)).toBe(12);
 });
+
+test('adds 7 + 8 to equal 15', () => {
+    expect(sum(7, 8)).toBe(15);
+});
